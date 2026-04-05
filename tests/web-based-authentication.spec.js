a47e8d390a8d810077479174b771f2f5deb6c86b
Bala Appana <bzappana@gmail.com>
2026-04-05 12:47:56 -0400

    Added Enviroments

--- tests/web-based-authentication.spec.js
+++ tests/web-based-authentication.spec.js
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test } from "@playwright/test";
 
 /** 
  * 
@@ -9,7 +9,6 @@ import { test } from '@playwright/test';
 
     await page.waitForTimeout(3000);
 
-  
 });
 
 */
